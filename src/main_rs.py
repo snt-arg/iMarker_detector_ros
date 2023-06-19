6cce771fbc895a53cd7dd89ac1c83f478eaffe22
Ali Tourani <a.tourani1991@gmail.com>
2023-06-19 17:09:41 +0200

    [feat] - add setup for mono

--- src/main_rs.py
+++ src/main_rs.py
@@ -3,7 +3,10 @@
 import rospy
 import cv2 as cv
 import numpy as np
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
 from src.csr_sensors.sensors import sensorRealSense
+from utils.valueParser import thresholdParser, channelParser
 from src.csr_detector.vision.channelSeparator import channelSeparator
 from config import realSenseResolution, realSenseFps, windowWidth
 
@@ -11,6 +14,13 @@ from config import realSenseResolution, realSenseFps, windowWidth
 def main():
     # Initializing a ROS node
     rospy.init_node('csr_detector_rsCam')
+    rate = rospy.Rate(10)  # Publishing rate in Hz
+    publisherCam = rospy.Publisher('camera', Image, queue_size=10)
+    publisherMask = rospy.Publisher('result_mask', Image, queue_size=10)
+    publisherResult = rospy.Publisher('result_frame', Image, queue_size=10)
+
+    # ROS Bridge
+    bridge = CvBridge()
 
     # Loading configuration values
     try:
@@ -18,45 +28,66 @@ def main():
     except:
         rospy.logerr("No Config file found!")
 
-    print(configs)
+    # Prepare the basic parameters
+    try:
+        isThreshOts, isThreshBoth, isThreshBin = thresholdParser(
+            configs['postProcessing']['thresholdMethod'])
+        isRChannel, isGChannel, isBChannel, isAllChannels = channelParser(
+            configs['preProcessing']['channel'])
+        # Prepare parameters based on what processor needs
+        params = {
+            'rChannel': isRChannel,
+            'gChannel': isGChannel,
+            'bChannel': isBChannel,
+            'threshbin': isThreshBin,
+            'threshots': isThreshOts,
+            'threshboth': isThreshBoth,
+            'allChannels': isAllChannels,
+            'windowWidth': configs['gui']['windowWidth'],
+            'isMarkerLeftHanded': configs['markers']['leftHanded'],
+        }
+    except:
+        rospy.logerr("Error in fetching parameters!")
 
-    # rs = sensorRealSense.rsCamera(realSenseResolution, realSenseFps)
+    # Camera
+    rs = sensorRealSense.rsCamera(realSenseResolution, realSenseFps)
 
-    # # Create a pipeline
-    # rs.createPipeline()
+    # Create a pipeline
+    rs.createPipeline()
 
-    # # Start the pipeline
-    # rs.startPipeline()
+    # Start the pipeline
+    rs.startPipeline()
 
-    # try:
-    #     while True:
-    #         event, values = window.read(timeout=10)
+    try:
+        while not rospy.is_shutdown():
 
-    #         # Wait for the next frames from the camera
-    #         frames = rs.grabFrames()
+            # Wait for the next frames from the camera
+            frames = rs.grabFrames()
 
-    #         # Get the color frame
-    #         colorFrame = rs.getColorFrame(frames)
+            # Get the color frame
+            colorFrame = rs.getColorFrame(frames)
 
-    #         # Get the values from the GUI
-    #         params = {'allChannels': values['AChannels'], 'rChannel': values['RChannel'],
-    #                   'gChannel': values['GChannel'], 'bChannel': values['BChannel'],
-    #                   'isMarkerLeftHanded': values['MarkerLeftHanded'], 'windowWidth': windowWidth
-    #                   }
+            # Change brightness
+            colorFrame = cv.convertScaleAbs(
+                colorFrame, alpha=configs['sensor']['brightness']['alpha'], beta=configs['sensor']['brightness']['beta'])
 
-    #         # Change brightness
-    #         colorFrame = cv.convertScaleAbs(
-    #             colorFrame, alpha=values['camAlpha'], beta=values['camBeta'])
+            procFrame = channelSeparator(colorFrame, params)
 
-    #         procFrame = channelSeparator(colorFrame, params)
+            # Show the frames
+            frame = cv.imencode(".png", procFrame)[1].tobytes()
 
-    #         # Show the frames
-    #         frame = cv.imencode(".png", procFrame)[1].tobytes()
+            # Continue publishing
+            rate.sleep()
 
-    # finally:
-    #     # Stop the pipeline and close the windows
-    #     rs.stopPipeline()
-    #     cv.destroyAllWindows()
+            # Convert to ROS
+            frameRos = bridge.cv2_to_imgmsg(procFrame, "bgr8")
+            colorFrameRos = bridge.cv2_to_imgmsg(colorFrame, "bgr8")
+            publisherResult.publish(frameRos)
+            publisherCam.publish(colorFrameRos)
+
+    finally:
+        # Stop the pipeline and close the windows
+        rs.stopPipeline()
 
 
 # Run the program
